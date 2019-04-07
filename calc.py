315fad4ccdf60b2766abaa32acbda72f08f78e8b
888hackprog888 <44940726+888hackprog888@users.noreply.github.com>
2019-04-07 17:37:55 +0500

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,19 @@
+﻿while 1==1:
+      print ("Calc.py")
+      onec=int(input("Enter onenumber  "))
+      twoc=int(input("Enter twonumber  "))
+      deistivie=input("Enter sign ")
+      if  deistivie=="+":
+            res1=onec+twoc
+            print(res1)
+      elif deistivie=="-":
+            res2=onec-twoc
+            print (res2)
+      elif deistivie=="*":
+            res3=onec*twoc
+            print (res3)
+      elif deistivie=="/":
+            res3=onec/twoc
+            print (res3)
+      else:
+            print ("ERROR!!")
